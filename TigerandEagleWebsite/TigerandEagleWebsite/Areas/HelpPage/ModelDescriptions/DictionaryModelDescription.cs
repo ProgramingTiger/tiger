e0c934f4f3449c018af952a4ce1149d8abcdfa17
EagleCraft <codya259@gmail.com>
2014-12-22 20:08:13 -0500

    added files

---
+++ TigerandEagleWebsite/TigerandEagleWebsite/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TigerandEagleWebsite.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TigerandEagleWebsite/TigerandEagleWebsite/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TigerandEagleWebsite.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
